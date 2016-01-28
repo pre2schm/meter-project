708d84483231ec63a49e08caa9d3d3d02e388027
Samuel Morris <samuel.morris@yahoo.co.uk>
2016-01-28 22:33:32 +0000

    string to json

--- server.js
+++ server.js
@@ -14,14 +14,21 @@ app.use(bodyParser.raw());
 app.use(bodyParser.json());
 app.use(bodyParser.text());
 
+var log = [];
 
 app.post('/api', function (req,res) {
   
-  var body = str2json.convert(req.body);
-  console.log(body);
+  //var body = str2json.convert(req.body);
+  var body = (req.body);
+  log.push(body);
+  console.log('logging!');
   return res.sendStatus(200).send();
 });
 
+
+app.get('/', function(req, res) {
+    res.send(log);
+});
 
 app.listen(PORT, function(){
   console.log('Express listening on port ' + PORT + '!');
